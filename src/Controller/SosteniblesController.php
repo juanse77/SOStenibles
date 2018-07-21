89ea7726e7e69837b40e28e743fcd71c20bc0239
juanse77 <juanseraar@hotmail.com>
2018-07-21 19:41:12 +0100

    3 commit

--- src/Controller/SosteniblesController.php
+++ src/Controller/SosteniblesController.php
@@ -8,11 +8,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class SosteniblesController extends Controller {
 
 	public function index() {
-		$number = random_int(0, 100);
-
-        return $this->render('sostenibles/number.html.twig', array(
-            'number' => $number,
-        ));
+        return $this->render('sostenibles/index.html.twig');
 	}
 
+	public function proyectos($page = 1) {
+        return $this->render('sostenibles/proyectos.html.twig', ['page' => $page]);
+	}
+	
+	public function contacto() {
+        return $this->render('sostenibles/contacto.html.twig');
+	}
+	
+	public function proyecto($id_proyecto) {
+        return $this->render('sostenibles/proyecto.html.twig', ['id_proyecto' => $id_proyecto]);
+	}
+	
+	public function home($id_usuario) {
+        return $this->render('sostenibles/home.html.twig', ['id_usuario' => $id_usuario]);
+	}
+	
+	public function logo($id_proyecto) {
+        return $this->render('sostenibles/logo.html.twig', ['id_proyecto' => $id_proyecto]);
+	}
 }
